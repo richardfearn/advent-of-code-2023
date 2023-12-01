5007d7b4df74748f35ec5bd937453c3909ac4adf
Richard Fearn <richardfearn@gmail.com>
2023-12-01 06:24:46 +0000

    Day 1: use regex

--- day1/__init__.py
+++ day1/__init__.py
@@ -1,5 +1,10 @@
+import re
+
 WORDS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-WORD_DIGITS = {word: str(i + 1) for (i, word) in enumerate(WORDS)}
+
+REGEX = f"(?=({'|'.join(WORDS)}|[1-9]))"
+
+VALUES = {word: i + 1 for (i, word) in enumerate(WORDS)} | {str(i): i for i in range(1, 10)}
 
 
 def part_1_answer(lines):
@@ -12,13 +17,8 @@ def part_1_answer(lines):
 
 def part_2_answer(lines):
     def calibration_value(line):
-        digits = []
-        for i, c in enumerate(line):
-            if c.isdigit():
-                digits.append(c)
-            for word, digit in WORD_DIGITS.items():
-                if line[i:i + len(word)] == word:
-                    digits.append(digit)
-        return int(digits[0] + digits[-1])
+        digits = re.findall(REGEX, line)
+        first, last = digits[0], digits[-1]
+        return VALUES[first] * 10 + VALUES[last]
 
     return sum(calibration_value(line) for line in lines)
