4de853d82d21fd7519552c34718417c2648567c3
Richard Fearn <richardfearn@gmail.com>
2023-12-11 09:20:04 +0000

    Day 11: better solution using modified Manhattan distance

--- day11/__init__.py
+++ day11/__init__.py
@@ -1,6 +1,3 @@
-from heapq import heappop, heappush
-
-
 def part_1_answer(lines):
     return answer(lines, 2)
 
@@ -16,8 +13,14 @@ def answer(grid, scale_factor):
     empty_rows, empty_columns = find_empty_rows_and_columns(grid, width, height)
     galaxies = find_galaxies(grid, width, height)
 
-    all_galaxy_distances = find_galaxy_distances(galaxies, empty_rows, empty_columns, scale_factor, width, height)
-    return sum(all_galaxy_distances) // 2
+    col_dists = step_dists(width, empty_columns, scale_factor)
+    row_dists = step_dists(height, empty_rows, scale_factor)
+
+    dists = []
+    for i in range(len(galaxies)):
+        for j in range(i + 1, len(galaxies)):
+            dists.append(distance(galaxies, i, j, col_dists, row_dists))
+    return sum(dists)
 
 
 def find_empty_rows_and_columns(grid, width, height):
@@ -44,35 +47,16 @@ def find_galaxies(grid, width, height):
     return galaxies
 
 
-def find_galaxy_distances(galaxies, empty_rows, empty_columns, scale_factor, width, height):
-    # pylint: disable=too-many-arguments,too-many-locals
-
-    all_galaxy_distances = []
-
-    for start_pos in galaxies:
-
-        dist = {start_pos: 0}
-        q = [(0, start_pos)]
-
-        while len(q) > 0:
-            u = heappop(q)[1]
-            for v in neighbours(u, width, height):
-                nx, ny = v
-                step_dist = scale_factor if ((ny in empty_rows) or (nx in empty_columns)) else 1
-                alt = dist[u] + step_dist
-                if (v not in dist) or (alt < dist[v]):
-                    dist[v] = alt
-                    heappush(q, (alt, v))
-
-        galaxy_distances = [dist[g] for g in galaxies]
-        all_galaxy_distances.extend(galaxy_distances)
-
-    return all_galaxy_distances
+def step_dists(length, empty_positions, scale_factor):
+    dists = [0]
+    for i in range(1, length):
+        dists.append(dists[-1] + (scale_factor if (i in empty_positions) else 1))
+    return dists
 
 
-def neighbours(pos, width, height):
-    x, y = pos
-    for ox, oy in (-1, 0), (1, 0), (0, -1), (0, 1):
-        nx, ny = (x + ox), (y + oy)
-        if (0 <= nx < width) and (0 <= ny < height):
-            yield nx, ny
+def distance(galaxies, i, j, col_dists, row_dists):
+    x1, y1 = galaxies[i]
+    x2, y2 = galaxies[j]
+    horizontal_dist = abs(col_dists[x2] - col_dists[x1])
+    vertical_dist = abs(row_dists[y2] - row_dists[y1])
+    return horizontal_dist + vertical_dist
